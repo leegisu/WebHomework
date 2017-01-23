0f349db014efb12a99dcb2c974e628cf403a2dd3
leegisu <newlee1962@gmail.com>
2017-01-23 13:06:45 +0900

    Update tabJS.js

--- tabJS.js
+++ tabJS.js
@@ -9,7 +9,7 @@ function excuteEvent() {
   var friend_div = document.querySelector("#friend");
   var theme_div = document.querySelector("#theme");
   var news_div = document.querySelector("#news");
-  //console.log("kk");
+  
   position_div.addEventListener("click", showEventType);
   friend_div.addEventListener("click", showEventType);
   theme_div.addEventListener("click", showEventType);
@@ -21,8 +21,7 @@ function showEventType(evt) {
 console.log(evt.toElement.parentNode.id);
 
 var div_name = evt.toElement.innerText;
-// var id_name = evt.toElement.parentNode.id;
-// debugger;
+
 if(div_name=="aboutMe"){
 
   var sendURL = "http://jsonplaceholder.typicode.com/posts/1";
@@ -30,7 +29,7 @@ if(div_name=="aboutMe"){
   ajaxtest(sendURL,this);
   }
 
-  //console.log(div_name);
+  
 }else if(div_name=="friend"){
 
   var sendURL = "http://jsonplaceholder.typicode.com/posts/2";
@@ -38,15 +37,15 @@ if(div_name=="aboutMe"){
   ajaxtest(sendURL ,this);
   }
 
-  //console.log(div_name);
+  
 }else if(div_name=="lorem"){
 
   var sendURL = "http://jsonplaceholder.typicode.com/posts/3";
   if(evt.type === "click") {
   ajaxtest(sendURL,this);
   }
-  //console.log(div_name);
-}else{
+  
+  }else{
 
   var sendURL = "http://jsonplaceholder.typicode.com/posts/4";
   if(evt.type === "click") {
@@ -74,17 +73,12 @@ function ajaxtest(url, div) {
 
 function insertContent(title,body,div) {
 
-  // var temp = "#" + id_name;
-
   var currentTab = document.querySelector(".selectedTab");
   currentTab.classList.remove("selectedTab");
-  //var tabColorReset2 = document.querySelector("#news");
+ 
   div.classList.add("selectedTab");
 
-  //console.log(temp);
-  // tabColorReset1.style.backgroundColor = "blue";
-  //tabColorReset2.style.backgroundColor = "white";
-//console.log("test");
+  
   var showTitle = document.querySelector(".myName");// 제목 바꾸기
   showTitle.innerHTML = title;
 
